c588c9c8a20f6cff645223c74761fd5f0a6e34af
Felipe Vidal <felipe.vidal@mesquita.rj.gov.br>
2023-10-06 10:54:13 -0300

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,27 +13,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $tecnologia = \App\Models\Setor::firstOrCreate([
+        $ponto = \App\Models\Setor::firstOrCreate([
             "id" => 1,
-            "nome" => "TECNOLOGIA",
-            "soma_entrada" => -1,
-            "soma_saida" => 1,
-        ]);
-
-        $tecnologia = \App\Models\Setor::firstOrCreate([
-            "id" => 2,
             "nome" => "PONTO",
             "soma_entrada" => 0,
             "soma_saida" => 0,
         ]);
 
+        // $tecnologia = \App\Models\Setor::firstOrCreate([
+        //     "id" => 2,
+        //     "nome" => "TECNOLOGIA",
+        //     "soma_entrada" => -1,
+        //     "soma_saida" => 1,
+        // ]);
+
         \App\Models\User::firstOrcreate([
-            "name" => "Felipe Vidal",
-            "email" => "felipe.vidal@mesquita.rj.gov.br",
+            "name" => "Super Admin",
+            "email" => "root@mesquita.rj.gov.br",
             "password" => Hash::make(config("app.user_default_password", "")),
-            "cpf"      => $request->cpf,
+            "cpf"      => "11111111111",
             "nivel"    => "Super-Admin",
-            "setor_id" => $tecnologia->id,
+            "setor_id" => $ponto->id,
         ]);
     }
 }
