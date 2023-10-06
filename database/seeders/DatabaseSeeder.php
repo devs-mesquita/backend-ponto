f34c30a29e88664653b066aa18bac97f79ff5d74
Felipe Vidal <felipe.vidal@mesquita.rj.gov.br>
2023-10-06 10:48:29 -0300

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $tecnologia = \App\Models\Setor::firstOrCreate([
-            "id" => 1
+            "id" => 1,
             "nome" => "TECNOLOGIA",
             "soma_entrada" => -1,
             "soma_saida" => 1,
