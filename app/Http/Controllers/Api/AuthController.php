9c92cedff19275d122cc7f4feec3cbe81ef96ba4
Felipe Vidal <vidalfelipe2018@gmail.com>
2023-11-05 18:33:17 -0300

    Backend

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,212 +7,250 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Carbon;
 
 class AuthController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api', ['except' => ['login', 'register', 'getUsersBySetor']]);
-    // }
-
-    public function login(Request $request)
-    {
-        $request->validate([
-            'email' => 'required|string|email',
-            'password' => 'required|string',
-        ]);
-        $credentials = $request->only('email', 'password');
-        $token = Auth::attempt($credentials);
-        
-        if (!$token) {
-            return response()->json([
-                'resultado' => 'unauthorized',
-            ], 401);
-        }
-
-        //$user = Auth::user();
-        $user = User::with('setor')->find(Auth::id());
-        return response()->json([
-            'user' => $user,
-            'default_password' => Hash::check(config('app.user_default_password', ''), $user->password),
-            'authorization' => [
-                'token' => $token,
-                'type' => 'bearer',
-                'expires_in' => 60 * 24 * 365.25
-            ]
-        ]);
-    }
-
-    public function register(Request $request)
-    {
-        if (User::where('cpf', $request->cpf)->count() > 0) {
+  public function login(Request $request)
+  {
+      $request->validate([
+          'email' => 'required|string|email',
+          'password' => 'required|string',
+      ]);
+      $credentials = $request->only('email', 'password');
+      $token = Auth::attempt($credentials);
+      
+      if (!$token) {
           return response()->json([
-            'resultado' => 'cpf-existente',
-          ], 400);
-        }
+              'resultado' => 'unauthorized',
+          ], 401);
+      }
 
-        if (User::where('email', $request->email)->count() > 0) {
-          return response()->json([
-            'resultado' => 'email-existente',
-          ], 400);
-        }
+      //$user = Auth::user();
+      $user = User::with('setor')->find(Auth::id());
+      return response()->json([
+          'user' => $user,
+          'default_password' => Hash::check(config('app.user_default_password', ''), $user->password),
+          'authorization' => [
+              'token' => $token,
+              'type' => 'bearer',
+              'expires_in' => 60 * 24 * 365.25
+          ]
+      ]);
+  }
 
-        $user = User::create([
-            'name'     => $request->name,
-            'email'    => $request->email,
-            'password' => Hash::make(config('app.user_default_password', '')),
-            'cpf'      => $request->cpf,
-            'nivel'    => $request->nivel,
-            'setor_id' => $request->setor_id,
-        ]);
-
+  public function register(Request $request)
+  {
+      if (User::where('cpf', $request->cpf)->count() > 0) {
         return response()->json([
-            'resultado' => 'created',
-            'user' => $user
-        ]);
-    }
+          'resultado' => 'cpf-existente',
+        ], 400);
+      }
 
-    public function logout()
-    {
-        Auth::logout();
+      if (User::where('email', $request->email)->count() > 0) {
         return response()->json([
-            'resultado' => 'ok',
-        ]);
-    }
+          'resultado' => 'email-existente',
+        ], 400);
+      }
 
-    public function refresh()
-    {
-        return response()->json([
-            'user' => User::with('setor')->find(Auth::id()),
-            'authorization' => [
-                'token' => auth()->refresh(),
-                'type' => 'bearer',
-                'expires_in' => 60 * 24 * 365.25
-            ]
-        ]);
-    }
+      $user = User::create([
+          'name' => $request->name,
+          'email' => $request->email,
+          'password' => Hash::make(config('app.user_default_password', '')),
+          'cpf' => $request->cpf,
+          'nivel' => $request->nivel,
 
-    public function getUsersBySetor($setor)
-    {
-      $users = User::with('setor')
-      ->where('setor_id', $setor)
-      ->orderBy('name', 'asc')
-      ->get();
+          'matricula' => $request->matricula,
+          'pispasep' => $request->pispasep,
+          'ctps' => $request->ctps,
+          'data_admissao' => $request->data_admissao ? Carbon::parse($request->data_admissao)->toDateTimeString() : null,
+          'cargo' => mb_strtoupper($request->cargo),
+          'lotacao' => mb_strtoupper($request->lotacao),
+          'repouso' => json_encode($request->repouso),
+
+          'setor_id' => $request->setor_id,
+      ]);
 
       return response()->json([
-        'users' => $users
+          'resultado' => 'created',
+          'user' => $user
+      ]);
+  }
+
+  public function updateUser(Request $request)
+  {
+    $user = User::find($request->user_id);
+
+    if ($user === null) {
+      return response()->json([
+        'resultado' => 'not-found',
+      ], 404);
+    }
+
+    $user->name = $request->name;
+    $user->email = $request->email;
+    $user->cpf = $request->cpf;
+    $user->nivel = $request->nivel;
+    
+    $user->matricula = $request->matricula;
+    $user->pispasep = $request->pispasep;
+    $user->ctps = $request->ctps;
+    $user->data_admissao = $request->data_admissao ? Carbon::parse($request->data_admissao)->toDateTimeString() : null;
+    $user->cargo = mb_strtoupper($request->cargo);
+    $user->lotacao = mb_strtoupper($request->lotacao);
+    $user->repouso = json_encode($request->repouso);
+
+    $user->setor_id = $request->setor_id;
+
+    $user->save();
+
+    return response()->json([
+        'resultado' => 'created',
+        'user' => $user
+    ]);
+  }
+
+  public function logout()
+  {
+      Auth::logout();
+      return response()->json([
+          'resultado' => 'ok',
+      ]);
+  }
+
+  public function refresh()
+  {
+      return response()->json([
+          'user' => User::with('setor')->find(Auth::id()),
+          'authorization' => [
+              'token' => auth()->refresh(),
+              'type' => 'bearer',
+              'expires_in' => 60 * 24 * 365.25
+          ]
+      ]);
+  }
+
+  public function getUsersBySetor($setor)
+  {
+    $users = User::with('setor')
+    ->where('setor_id', $setor)
+    ->orderBy('name', 'asc')
+    ->get();
+
+    return response()->json([
+      'users' => $users
+    ]);
+  }
+
+  public function resetPassword(Request $request) {
+    $user = User::with('setor')->find($request?->user_id);
+
+    if ($user->nivel === "Super-Admin" && in_array(auth()->user()->nivel(), ["Admin", "User"])) {
+      return response()->json([
+        'resultado' => 'unauthorized',
+      ], 403);
+    }
+
+    if ($user === null) {
+      return response()->json([
+        'resultado' => 'not-found',
+      ], 400);
+    }
+    
+    $user->password = Hash::make(config('app.user_default_password', ''));
+    
+    if(auth()->user()->setor_id !== $user->setor_id && auth()->user()->nivel !== 'Super-Admin') {
+      return response()->json([
+        'resultado' => 'unauthorized'
+      ], 403);
+    }
+
+    $user->save();
+
+    return response()->json([
+      'resultado' => 'ok',
+    ], 200);
+  }
+
+  public function changeNivel(Request $request) {
+    $user = User::with('setor')->find($request?->user_id);
+
+    if ($user === null) {
+      return response()->json([
+        'resultado' => 'not-found',
+      ], 400);
+    }
+
+    $user->nivel = $request->nivel;
+    $user->save();
+
+    return response()->json([
+      'resultado' => 'ok',
+    ], 200);
+  }
+
+  public function changeSetor(Request $request) {
+    $user = User::find($request?->user_id);
+
+    if ($user === null) {
+      return response()->json([
+        'resultado' => 'not-found',
+      ], 400);
+    }
+
+    $user->setor_id = $request->setor_id;
+    $user->save();
+
+    return response()->json([
+      'resultado' => 'ok',
+    ], 200);
+  }
+
+  public function changePassword(Request $request) {
+    if ($request->newPassword !== $request->confirmPassword) {
+      return response()->json([
+        'resultado' => 'wrong-confirm-password',
+      ], 400);
+    }
+    
+    $user = User::with('setor')->find(auth()->user()->id);
+
+    if ($user === null) {
+      return response()->json([
+        'resultado' => 'not-found',
+      ], 400);
+    }
+
+    if (!Hash::check($request->currentPassword, auth()->user()->password)) {
+      return response()->json([
+        'resultado' => 'wrong-current-password',
+      ], 403);
+    }
+
+    $user->password = Hash::make($request->newPassword);
+    $user->save();
+
+    return response()->json([
+      'resultado' => 'ok',
+    ], 200);
+  }
+
+  public function checkDefaultPassword(Request $request)
+  {
+    $user = User::find(auth()?->user()?->id);
+      
+    if ($user === null) {
+      return response()->json([
+        'resultado' => 'not-found',
       ]);
     }
 
-    public function resetPassword(Request $request) {
-      $user = User::with('setor')->find($request?->user_id);
-
-      if ($user->nivel === "Super-Admin" && in_array(auth()->user()->nivel(), ["Admin", "User"])) {
-        return response()->json([
-          'resultado' => 'unauthorized',
-        ], 403);
-      }
-
-      if ($user === null) {
-        return response()->json([
-          'resultado' => 'not-found',
-        ], 400);
-      }
-      
-      $user->password = Hash::make(config('app.user_default_password', ''));
-      
-      if(auth()->user()->setor_id !== $user->setor_id && auth()->user()->nivel !== 'Super-Admin') {
-        return response()->json([
-          'resultado' => 'unauthorized'
-        ], 403);
-      }
-
-      $user->save();
-
+    if (Hash::check(config('app.user_default_password', ''), $user->password)) {
       return response()->json([
-        'resultado' => 'ok',
-      ], 200);
+        'resultado' => 'default-password',
+      ]);
     }
 
-    public function changeNivel(Request $request) {
-      $user = User::with('setor')->find($request?->user_id);
-
-      if ($user === null) {
-        return response()->json([
-          'resultado' => 'not-found',
-        ], 400);
-      }
-
-      $user->nivel = $request->nivel;
-      $user->save();
-
-      return response()->json([
-        'resultado' => 'ok',
-      ], 200);
-    }
-
-    public function changeSetor(Request $request) {
-      $user = User::find($request?->user_id);
-
-      if ($user === null) {
-        return response()->json([
-          'resultado' => 'not-found',
-        ], 400);
-      }
-
-      $user->setor_id = $request->setor_id;
-      $user->save();
-
-      return response()->json([
-        'resultado' => 'ok',
-      ], 200);
-    }
-
-    public function changePassword(Request $request) {
-      if ($request->newPassword !== $request->confirmPassword) {
-        return response()->json([
-          'resultado' => 'wrong-confirm-password',
-        ], 400);
-      }
-      
-      $user = User::with('setor')->find(auth()->user()->id);
-
-      if ($user === null) {
-        return response()->json([
-          'resultado' => 'not-found',
-        ], 400);
-      }
-
-      if (!Hash::check($request->currentPassword, auth()->user()->password)) {
-        return response()->json([
-          'resultado' => 'wrong-current-password',
-        ], 403);
-      }
-
-      $user->password = Hash::make($request->newPassword);
-      $user->save();
-
-      return response()->json([
-        'resultado' => 'ok',
-      ], 200);
-    }
-
-    public function checkDefaultPassword(Request $request)
-    {
-      $user = User::find(auth()?->user()?->id);
-        
-      if ($user === null) {
-        return response()->json([
-          'resultado' => 'not-found',
-        ]);
-      }
-
-      if (Hash::check(config('app.user_default_password', ''), $user->password)) {
-        return response()->json([
-          'resultado' => 'default-password',
-        ]);
-      }
-
-      return response()->json(['resultado' => 'ok']);
-    }
+    return response()->json(['resultado' => 'ok']);
+  }
 }
--- app/Http/Controllers/Api/SetoresController.php
+++ app/Http/Controllers/Api/SetoresController.php
@@ -29,6 +29,20 @@ class SetoresController extends Controller
 
       $setor = new Setor;
       $setor->nome          = $nome;
+      
+      $setor->empresa          = $empresa;
+      $setor->cnpj          = $cnpj;
+      $setor->cnae          = $cnae;
+      $setor->visto_fiscal          = $visto_fiscal;
+      
+      $setor->logradouro          = $logradouro;
+      $setor->numero_logradouro          = $numero_logradouro;
+      $setor->complemento          = $complemento;
+      $setor->bairro          = $bairro;
+      $setor->cidade          = $cidade;
+      $setor->uf          = $uf;
+      $setor->cep          = $cep;
+
       $setor->soma_saida    = $request->soma_saida;
       $setor->soma_entrada  = $request->soma_entrada;
       $setor->save();
@@ -37,5 +51,40 @@ class SetoresController extends Controller
           'resultado' => 'ok',
           ]
       );
+    }
+
+    public function update(Request $request)
+    {
+      $setor = Setor::find($request->setor_id);
+
+      if ($setor === null) {
+        return response()->json([
+          'resultado' => 'not-found'
+        ], 404);
+      }
+
+      $setor->nome          = $request->nome;
+
+      $setor->empresa          = $request->empresa;
+      $setor->cnpj          = $request->cnpj;
+      $setor->cnae          = $request->cnae;
+      $setor->visto_fiscal          = $request->visto_fiscal;
+
+      $setor->logradouro          = $request->logradouro;
+      $setor->numero_logradouro          = $request->numero_logradouro;
+      $setor->complemento          = $request->complemento;
+      $setor->bairro          = $request->bairro;
+      $setor->cidade          = $request->cidade;
+      $setor->uf          = $request->uf;
+      $setor->cep          = $request->cep;
+
+      $setor->soma_saida    = $request->soma_saida;
+      $setor->soma_entrada  = $request->soma_entrada;
+
+      $setor->save();
+
+      return response()->json([
+        'resultado' => 'ok',
+      ]);
     }
 }
